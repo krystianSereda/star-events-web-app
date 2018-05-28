fa555d960b3415aad1393cf8b5d24e8f8de2e3ef
Esteban Vega <stron@me.com>
2018-05-28 12:30:03 +0200

    -Deleted the date widget -Added fixed editor window to the right -When pressing 'mehr' then the editor gets the info and can be modified (it is not yet saved)

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,3 @@
-
-
-
 var objectCount = 0;
 
 // add text input for object
@@ -14,4 +11,79 @@ function addObject(){
 function removeObject(){
     $('input[name="event_obj' + objectCount + '"]').remove();
     objectCount--;
+}
+
+// fill the forms in the editor
+function handleEditButtonPressed(id){
+    // get the info
+    var elem = document.getElementById(id)
+    const objNum = elem.children[1].childElementCount
+
+    const in_date = elem.children[0].children[0].innerHTML;
+    const in_place = elem.children[0].children[1].innerHTML;
+    
+    
+
+    // get the inputs
+    var date = document.getElementById('editor_date');
+    var plac = document.getElementById('editor_place');
+    var objs = document.getElementById('editor_objects');
+
+    // enable them
+    date.disabled = false;
+    plac.disabled = false;
+    document.getElementById('add_b').disabled = false;
+    document.getElementById('del_b').disabled = false;
+    document.getElementById('end_b').disabled = false;
+
+
+
+
+    // set the info
+    date.value = in_date;
+    plac.value = in_place;
+
+    var inputs = "";
+    const inp = '<input type="text"';
+
+    if(objNum > 0){
+        for(var i = 0; i < objNum; i++){
+            const v = elem.children[1].children[i].children[0].innerHTML;
+            const val = inp + 'value="' + v +'">';
+            inputs += val;
+        }
+
+        objs.innerHTML = inputs;
+    }
+    else{
+        objs.innerHTML = inp + '>';
+    }
+    
+    
+}
+
+
+var editObjectCount = 1;
+function handleAddObjectPressed(){
+    console.log('add object');
+    var inputs = "";
+    const inp = '<input type="text">';
+
+    editObjectCount++;
+
+    for(var i = 0; i < editObjectCount; i++){
+        inputs += inp;
+    }
+
+    document.getElementById('editor_objects').innerHTML = inputs;
+}
+
+function handleDeleteObjectPressed(){
+    editObjectCount -= 2;
+    handleAddObjectPressed();
+}
+
+function handleSaveEditPressed(){
+    console.log('save edit');
+    
 }
