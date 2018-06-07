da93694e88e42885596dd7422c913f22a4a4b6aa
Esteban Vega <stron@me.com>
2018-06-07 23:21:47 +0200

    - deleted extra template form.ejs - checkFields checks that the proper date is put in - date input is now three number fields (day, month, year) - error css class for editor inputs

--- public/js/editor.js
+++ public/js/editor.js
@@ -8,18 +8,53 @@ function DOM(id){
     return document.getElementById(id);
 }
 
+// check for min max values for dates
+function checkMinMax(elem){
+    const max = parseInt(elem.max)
+    const min = parseInt(elem.min)
+    
+    if(elem.value <= max && elem.value >= min){
+        return
+    }
+
+    if(elem.value < min){
+        elem.value = min
+        return
+    }
+    
+    if(elem.value > max){
+        elem.value = max
+        return
+    }
+}
+
 // enable / disable the editor input fields, and optionally
 // fill them with a given value
 function toggleEditorInputs(disable, inDate=null, inPlace=null){
-    var date = DOM('editor_date');
+    var day = DOM('editor_day')
+    var month = DOM('editor_month')
+    var year = DOM('editor_year')
+
     var plac = DOM('editor_place');
     if(inDate != null){
-        date.value = inDate
+        if(inDate != ""){
+            const date = inDate.split('-')
+            day.value = parseInt(date[2])
+            month.value = parseInt(date[1])
+            year.value = parseInt(date[0])
+        }
+        else{
+            day.value = ""
+            month.value = ""
+            year.value = ""
+        }
     }
     if(inPlace != null){
         plac.value = inPlace
     }
-    date.disabled = disable;
+    day.disabled = disable;
+    month.disabled = disable;
+    year.disabled = disable;
     plac.disabled = disable;
     DOM('add_b').disabled = disable;
     DOM('del_b').disabled = disable;
@@ -89,27 +124,57 @@ function updateList(){
 // check the editor fields, 
 // returns true if it's good to go, else false
 function checkFields(){
+    var everythingGood = true
+
     // date
-    const dateval = DOM('editor_date').value;
-    var date = new Date(dateval)
-    const isDate = date.toString() != "Invalid Date"
-    
-    if(!dateval || dateval == "" || !isDate){
-        console.log('date');
-        
-        return false
+    const year = DOM('editor_year').value
+    const month = DOM('editor_month').value
+    const day = DOM('editor_day').value
+
+    if(year != null && month != null && day != null){
+        // padding with 0's
+        m = month.toString().length < 2 ? "0" + month : month
+        d = day.toString().length < 2 ? "0" + day : day
+
+        var dateval = ""
+        dateval += "" + year
+        dateval += "-" + m
+        dateval += "-" + d
+
+        var date = new Date(dateval)
+        const isDate = date.toString() != "Invalid Date"
+
+        if(!isDate){
+            DOM("editor_day").classList.add('error-field')
+            DOM("editor_month").classList.add('error-field')
+            DOM("editor_year").classList.add('error-field')
+            everythingGood = false
+            console.log(dateval);
+            
+        }
+
     }
+    else{
+        DOM("editor_day").classList.add('error-field')
+        DOM("editor_month").classList.add('error-field')
+        DOM("editor_year").classList.add('error-field')
+        everythingGood = false
+        console.log('t');
+        
+    }
+
 
     // place
     // for now, only check if its not empty
     const locval = DOM("editor_place").value
     if(!locval || locval == ""){
-        console.log('place');
+        DOM("editor_place").classList.add('error-field')
+        everythingGood = false
+        console.log('w');
         
-        return false
     }
 
-    return true
+    return everythingGood
 }
 
 
@@ -248,17 +313,20 @@ function handleDeleteObjectPressed(){
 function handleSaveEditPressed(){
     // check the fields
     if(!checkFields()){
-        alert("Fields incorrectly filled!")
         return
     }
 
-    var date = DOM('editor_date');
+    
+    var date = ""
+    date += "" + DOM('editor_year').value
+    date += "-" + DOM('editor_month').value
+    date += "-" + DOM('editor_day').value
     var plac = DOM('editor_place');
     var objs = DOM('editor_objects').childNodes;
     const ID = DOM('editor_id').value.split('_')[2];
     
     const data = {
-        event_date: date.value,
+        event_date: date,
         event_loc: plac.value,
         id: ID
     }
@@ -293,6 +361,12 @@ function handleCancelEditPressed(){
     // delete button
     DOM('los_b').style.display = "none";
     DOM('los_b').classList.remove("fade-in")
+
+    DOM('editor_place').classList.remove('error-field')
+    DOM('editor_day').classList.remove('error-field')
+    DOM('editor_month').classList.remove('error-field')
+    DOM('editor_year').classList.remove('error-field')
+
 }
 
 // create a new entry
